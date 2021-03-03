15ecb7e18fd1fddc271994d418c15d6d8f610ab7
VitalijCsharp <kv1tosv@gmail.com>
2021-03-03 12:04:50 +0200

    Add project files.

---
+++ DailyRate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double fee = calculateFee(theDailyRate: 450);
+            Console.WriteLine($"Fee = {fee}");
+        }
+
+        
+        double calculateFee(double theDailyRate = 500, int noOfDays = 1)
+        {
+           Console.WriteLine("calculateFee using tow optional parameters");
+           return theDailyRate * noOfDays;
+        }
+        double calculateFee(double dailyRate = 500)
+        {
+           Console.WriteLine("calculateFee using one optional parameter");
+           int defaultNoOfDays = 1;
+           return defaultNoOfDays * dailyRate;
+        }
+        double calculateFee()
+        {
+           Console.WriteLine("calculateFee using hardcoded value");
+           int defaultNoOfDays = 1;
+           double defaultDailyRate = 400;
+           return defaultNoOfDays * defaultDailyRate;
+        }
+
+
+    }
+}
